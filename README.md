c5100d3ccd75d2ebc2b203cb3c0f4f72d491c7f2
Noah Mutersbaugh <mutersbn@mail.gvsu.edu>
2020-06-30 11:59:11 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# smite-god-builder
+ 
---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="hello">
+    <h1>{{ msg }} </h1>
+</template>
+
+<script>
+export default {
+    name: 'App',
+    data() {
+        return {
+            msg: 'Welcome to the web app!'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  width: 100%;
+  height: 100%;
+  padding: 25% 10%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-flow: column nowrap;
+  text-align: center;
+  font-size: 3vh;
+}
+</style>
