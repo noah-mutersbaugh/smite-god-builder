02e90fe525ca09820942256a9d039f070eca9fce
Hannah Cain <hcsch18@gmail.com>
2020-07-03 13:20:57 -0400

    Trying to get the router to work, attempt 2

--- src/App.vue
+++ src/App.vue
@@ -59,7 +59,7 @@
                             <div class="d-flex justify-content-between align-items-center">
                                 <div class="btn-group">
                                 <!-- <button type="button" class="btn btn-sm btn-outline-secondary">View</button> -->
-                                <router-link to='/Achilles' tag="button">View</router-link>
+                                <router-link to="/Achilles.vue" tag="button">View</router-link>
                                 <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                                 </div>
                                 <small class="text-muted">9 mins</small>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,15 @@ Vue.config.productionTip = false; // Disables "Running in development mode." not
 import 'bootstrap/dist/css/bootstrap.css';
 import './assets/global.scss';
 
+//creating routers to different pages 
+import VueRouter from 'vue-router';
+import routes from './routes';
+
+Vue.use(VueRouter);
+const router = new VueRouter({routes});
+
 // Create the app and place inside the HTML structure.
 new Vue({
-    render: h => h(App)
+    render: h => h(App),
+    routes
   }).$mount('#app');
