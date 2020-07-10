14fa986e099f9d83afff4cf098ba626efa943cf8
Hannah Cain <hcsch18@gmail.com>
2020-07-10 01:40:16 -0400

    Updates Achilles page

--- src/component/Achilles.vue
+++ src/component/Achilles.vue
@@ -1,87 +1,146 @@
 <template>
-    <div id="component" class="Achilles">
-        <!-- Custom styles for this template -->
-        <link href="assets/album.css" rel="stylesheet">
+  <div id="component" class="Achilles">
+    <!-- Custom styles for this template -->
+    <link href="assets/album.css" rel="stylesheet" />
 
-            <header>
-                <div class="collapse bg-dark" id="navbarHeader">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-sm-8 col-md-7 py-4">
-                                <h4 class="text-white">About</h4>
-                                <p class="text-muted">Add some information about the album below, the author, or any
-                                    other background context. Make it a few sentences long so folks can pick up some
-                                    informative tidbits. Then, link them off to some social networking sites or contact
-                                    information.</p>
-                            </div>
-                            <div class="col-sm-4 offset-md-1 py-4">
-                                <h4 class="text-white">Contact</h4>
-                                <ul class="list-unstyled">
-                                    <li><a href="#" class="text-white">Follow on Twitter</a></li>
-                                    <li><a href="#" class="text-white">Like on Facebook</a></li>
-                                    <li><a href="#" class="text-white">Email me</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="navbar navbar-dark bg-dark shadow-sm">
-                    <div class="container d-flex justify-content-between">
-                        <a href="#" class="navbar-brand d-flex align-items-center">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
-                                 stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                 aria-hidden="true" class="mr-2" viewBox="0 0 24 24" focusable="false">
-                                <path
-                                    d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"/>
-                                <circle cx="12" cy="13" r="4"/>
-                            </svg>
-                            <strong>Smite God Builder</strong>
-                        </a>
-                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
-                                aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                    </div>
-                </div>
-            </header>
+    <header>
+      <div class="collapse bg-dark" id="navbarHeader">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+              <h4 class="text-white">About</h4>
+              <p class="text-muted">
+                Add some information about the album below, the author, or any
+                other background context. Make it a few sentences long so folks can pick up some
+                informative tidbits. Then, link them off to some social networking sites or contact
+                information.
+              </p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+              <h4 class="text-white">Contact</h4>
+              <ul class="list-unstyled">
+                <li>
+                  <a href="#" class="text-white">Follow on Twitter</a>
+                </li>
+                <li>
+                  <a href="#" class="text-white">Like on Facebook</a>
+                </li>
+                <li>
+                  <a href="#" class="text-white">Email me</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="navbar navbar-dark bg-dark shadow-sm">
+        <div class="container d-flex justify-content-between">
+          <a href="#" class="navbar-brand d-flex align-items-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="none"
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              aria-hidden="true"
+              class="mr-2"
+              viewBox="0 0 24 24"
+              focusable="false"
+            >
+              <path
+                d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z"
+              />
+              <circle cx="12" cy="13" r="4" />
+            </svg>
+            <strong>Smite God Builder</strong>
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarHeader"
+            aria-controls="navbarHeader"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+        </div>
+      </div>
+    </header>
 
-            <main role="main">
-                <section className="jumbotron text-center">
-                    <div className="container">
-                        <h1>The builder parts</h1>
-                        <p className="lead text-muted"> Something lovely about the collection </p>
-                        <p>
-                            <button type="button" class="btn btn-sm btn-outline-secondary" @click="$router.push('/')">Back to home</button>
-                        </p>
-                    </div>
-                </section>
-            </main>
+    <main role="main">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <h1>The builder parts</h1>
+            <p class="lead text-muted">Something lovely about the collection</p>
+            <!-- <button type="button" class="btn btn-sm btn-outline-secondary" @click="$router.push('/')">Back to home</button> -->
+            <img
+              left
+              src="../images/achilles.jpg"
+              class="img-rounded"
+              alt="achilles"
+              width="100%"
+              height="225"
+              v-on:click="home"
+            />
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 shadow-sm">
+              <b-dropdown text="Select Items">
+                <b-dropdown-item href="#">An item</b-dropdown-item>
+                <b-dropdown-item href="#">Another item</b-dropdown-item>
+              </b-dropdown>
+              <div class="d-flex justify-content-between align-items-center">
+                <b-img v-bind="mainProps" alt="Transparent image"></b-img>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4">
+            <h1>Stats section</h1>
+            <blockquote
+              class="blockquote"
+            >working on 3 tables: offense, defense and utility updatable by the items selected throught the dropdown menu.</blockquote>
+          </div>
+        </div>
+      </div>
+    </main>
 
-            <footer className="text-muted">
-                <div className="container">
-                    <p className="float-right">
-                        <a href="#">Back to top</a>
-                    </p>
-                    <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-                    <p>New to Bootstrap? <a href="https://getbootstrap.com/">Visit the homepage.</a></p>
-                </div>
-            </footer>
-    </div>
+    <footer class="text-muted">
+      <div class="container">
+        <p class="float-right">
+          <a href="#">Back to top</a>
+        </p>
+        <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+        <p>
+          New to Bootstrap?
+          <a href="https://getbootstrap.com/">Visit the homepage.</a>
+        </p>
+      </div>
+    </footer>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'achilles',
-    data() {
-        return {
-            msg: 'Welcome to the web app!'
-        }
-    }
-}
+  name: "achilles",
+  data() {
+    return {
+      mainProps: { blank: true, width: 75, height: 75, class: "m1" }
+    };
+  }
+};
 </script>
 
 <style lang="scss">
- #app, #component {
+#app,
+#component {
   width: 100%;
   height: 100%;
   display: flex;
@@ -92,21 +151,23 @@ export default {
 }
 
 .bd-placeholder-img {
-font-size: 1.125rem;
-text-anchor: middle;
--webkit-user-select: none;
--moz-user-select: none;
--ms-user-select: none;
-user-select: none;
+  font-size: 1.125rem;
+  text-anchor: middle;
+  -webkit-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 }
 
 @media (min-width: 768px) {
-    .bd-placeholder-img-lg {
-        font-size: 3.5rem;
-    }
+  .bd-placeholder-img-lg {
+    font-size: 3.5rem;
+  }
 }
 
-header, main, footer {
-    width: 100%;
+header,
+main,
+footer {
+  width: 100%;
 }
 </style>
