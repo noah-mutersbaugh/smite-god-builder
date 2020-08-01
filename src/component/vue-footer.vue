37e30703e2ef6fe01fa0b2d01ddba8acd473c28d
Hannah Cain <hcsch18@gmail.com>
2020-07-31 23:52:35 -0400

    Cleans up header and footer

--- src/component/vue-footer.vue
+++ src/component/vue-footer.vue
@@ -4,11 +4,7 @@
       <p class="float-right">
         <a href="#">Back to top</a>
       </p>
-      <p>Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
-      <p>
-        New to Bootstrap?
-        <a href="https://getbootstrap.com/">Visit the homepage.</a>
-      </p>
+      <p>Website template used from &copy; Bootstrap</p>
     </div>
   </footer>
 </template>
