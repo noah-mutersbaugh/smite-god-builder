e97470074e15b29f2f65dbf0d9e554c5c0ff1fd6
mutersbn <mutersbn@mail.gvsu.edu>
2020-08-03 13:19:22 -0400

    removed footer that shouts out bootstrap

--- src/component/vue-footer.vue
+++ src/component/vue-footer.vue
@@ -2,9 +2,9 @@
   <footer class="text-muted">
     <div class="container">
       <p class="float-right">
-        <a href="#">Back to top</a>
+        <!-- <a href="#">Back to top</a> -->
       </p>
-      <p>Website template used from &copy; Bootstrap</p>
+      <!-- <p>Website template used from &copy; Bootstrap</p> -->
     </div>
   </footer>
 </template>
